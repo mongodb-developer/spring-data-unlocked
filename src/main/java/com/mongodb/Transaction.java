1eb3bad0a658a59c2c6b25cab53fc6879070c08f
Ricardo Mello <ricardo.mello@mongodb.com>
2024-11-27 15:50:28 -0300

    adjusting Transaction document

--- src/main/java/com/mongodb/Transaction.java
+++ src/main/java/com/mongodb/Transaction.java
@@ -2,13 +2,15 @@ package com.mongodb;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Document(collection = "transactions")
+@CompoundIndex(name = "type_currency", def = "{'transactionType': 1, 'currency': 1}")
 @Data
-
 public class Transaction {
     @Id
     private String id;
@@ -17,12 +19,20 @@ public class Transaction {
     private String currency;
     private String status;
     private String description;
-    private LocalDateTime createdAt;
     private AccountDetails accountDetails;
+
+    @Indexed(unique = true)
+    private String authCode;
+
+    @Indexed(
+            name = "PartialIndex",
+            expireAfterSeconds = 50,
+            partialFilter = "{ 'status': { $in: ['SUCCESS', 'COMPLETED'] } }"
+    )
+    private LocalDateTime createdAt;
 
     public record AccountDetails(Originator originator, Beneficiary beneficiary) {}
     public record Originator(String accountNumber, String name, String bank) {}
     public record Beneficiary(String accountNumber, String name, String bank) {}
-
 }
 
